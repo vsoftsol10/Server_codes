fa68f4c281626435fbad8272c91385ac7894bbff
Nirmal <nirmalsep2001@gmail.com>
2026-04-06 11:37:51 +0530

    Done

--- Vconstech_frontend/src/utils/tabToken.js
+++ Vconstech_frontend/src/utils/tabToken.js
@@ -1,9 +1,16 @@
-if (!sessionStorage.getItem('tabId')) {
-  sessionStorage.setItem('tabId', crypto.randomUUID());
-}
-const tabId = sessionStorage.getItem('tabId');
+// src/utils/tabToken.js
 
-export const tokenKey = `token_${tabId}`;
-export const getToken = () => localStorage.getItem(tokenKey);
-export const setToken = (token) => localStorage.setItem(tokenKey, token);
-export const removeToken = () => localStorage.removeItem(tokenKey);
+const getTabId = () => {
+  let tabId = sessionStorage.getItem('tabId');
+  if (!tabId) {
+    tabId = crypto.randomUUID();
+    sessionStorage.setItem('tabId', tabId);
+  }
+  return tabId;
+};
+
+export const tokenKey = () => `token_${getTabId()}`;
+
+export const getToken = () => localStorage.getItem(tokenKey());
+export const setToken = (token) => localStorage.setItem(tokenKey(), token);
+export const removeToken = () => localStorage.removeItem(tokenKey());

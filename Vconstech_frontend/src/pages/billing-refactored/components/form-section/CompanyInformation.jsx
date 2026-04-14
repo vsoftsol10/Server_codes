f71a2d77b839bb040a57644beebbbe8d9f15b78c
Nirmal <nirmalsep2001@gmail.com>
2026-04-14 12:51:30 +0530

    Changes URLs

--- Vconstech_frontend/src/pages/billing-refactored/components/form-section/CompanyInformation.jsx
+++ Vconstech_frontend/src/pages/billing-refactored/components/form-section/CompanyInformation.jsx
@@ -26,7 +26,7 @@ const CompanyInformation = ({ formData, handleInputChange }) => {
     setHasFetched(true);
     
     try {
-      const API_URL = import.meta.env.VITE_API_URL || '  http://localhost:5000/api';
+      const API_URL = import.meta.env.VITE_API_URL || 'https://test.vconstech.in/api';
       const userId = localStorage.getItem('userId');
       const token = getToken();
 
